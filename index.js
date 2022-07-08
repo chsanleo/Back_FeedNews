1270c62f2c6645eba649cab9ac283785050b3a7c
Christian <chsanleo@gmail.com>
2022-07-08 20:50:23 +0200

    load logger

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+//#region Logger
+const logger = require('./config/log4js-config.js');
+var log4js = require('log4js');
+
+app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
+//#endregion
 require('dotenv').config();
 
 app.use(cors());
