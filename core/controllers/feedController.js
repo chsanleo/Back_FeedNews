11a6b0b398a3fd3fdf429eaf2e3e40a6df1b0a33
Christian <chsanleo@gmail.com>
2022-07-09 19:12:36 +0200

    create class variable for error message

--- core/controllers/feedController.js
+++ core/controllers/feedController.js
@@ -2,6 +2,8 @@ const _feedService = require('../services/feedService.js');
 const conversionToModel = require('../model/conversionToModel.js');
 const conversionReturn = require('../model/conversionToReturn.js');
 
+let errorMsg = 'Error, contact with the Administrator.';
+
 const feedController = {
    
 };
