45bfd030f1aa96b7ba9c8e1cd6c8b3c371713b7c
Christian <chsanleo@gmail.com>
2022-07-10 01:17:22 +0200

    add information for swagger in router for create method

--- routers/feedRouter.js
+++ routers/feedRouter.js
@@ -1,6 +1,51 @@
 const router = require('express').Router();
 const feedController = require('../core/controllers/feedController.js');
 
+
+/**
+ * @swagger
+ * /feed/create:
+*    post:
+*     description: Create a new Feed 
+*     parameters:
+*       - name: body
+*         in: body
+*         required: true
+*         schema:
+*           type: object
+*           required:
+*             - title
+*             - body
+*             - source
+*             - publisher
+*           properties:
+*             title:
+*               type: string
+*             body:
+*               type: string
+*             image:
+*               type: string
+*             imageLink:
+*               type: string
+*             source:
+*               type: string
+*             publisher:
+*               type: string
+*           example: {
+*             "title": "Vacaciones en Agosto",
+*             "body": "Las vacaciones en esta epoca del año ...",
+*             "image": "Las vacaciones en esta epoca del año ...",
+*             "imageLink": "http://periodico.es/imagenes/imagen.png",
+*             "source": "http://periodico.es/vacaciones_agosto",
+*             "publisher": "El periodico", 
+*           }
+*     responses:
+*       201:
+*         schema:
+*           type: object
+*       500:
+ *         description: Error, contact with the Administrator.
+*/
 router.post('/create', feedController.create);
 router.post('/', feedController.get);
 router.get('/', feedController.getAll);
