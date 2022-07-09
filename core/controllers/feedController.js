39b2fd4d1efeb32b01464fec89e380eed22ed407
Christian <chsanleo@gmail.com>
2022-07-09 19:13:08 +0200

    create createfeed method for feedController

--- core/controllers/feedController.js
+++ core/controllers/feedController.js
@@ -5,7 +5,15 @@ const conversionReturn = require('../model/conversionToReturn.js');
 let errorMsg = 'Error, contact with the Administrator.';
 
 const feedController = {
-   
+    async create(req, res){
+        try {
+            let feed = await _feedService.create(conversionToModel.data_To_feed(req.body.data));
+            res.status(201).send(conversionReturn.feed_To_return(feed));
+        } catch(error) {
+            global.loggererror.error(error.message);
+            res.status(500).send({ message: errorMsg });
+        }
+    },
 };
 
 module.exports = feedController;
