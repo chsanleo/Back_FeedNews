57d36c153d0fa15e9f63ca744273f5be61ed9597
Christian <chsanleo@gmail.com>
2022-07-09 19:10:30 +0200

    create feed method

--- core/services/feedService.js
+++ core/services/feedService.js
@@ -2,7 +2,12 @@ const _feedRespository = require('../repositories/feedRepository.js');
 const Validations = require('../utils/validations.js');
 
 const FeedService = {
- 
+    async create(feed) {
+        try {
+            feed.createAt = Date.now().toString();
+            return await _feedRespository.create(feed);
+        } catch (error) { throw Error('Create Feed -- ' + error.message); }
+    },
 };
 
 module.exports = FeedService;
