730fdb0ecb9f54cb46ad38048d8dfd97049dbd32
Christian <chsanleo@gmail.com>
2022-07-10 01:12:43 +0200

    add tostorage the date in sql and remove return from C U D

--- core/repositories/feedRepository.js
+++ core/repositories/feedRepository.js
@@ -1,11 +1,18 @@
 const Feed = require('../database/models/Feed.js');
+const Utils = require('../utils/utils.js');
 
 const feedRepository = {
-    async create (feed){ return await Feed.create(feed); }, 
+    async create (feed){ 
+        feed.createAt = Utils.dateNowSQL();
+         await Feed.create(feed);
+    }, 
     async get (_id) { return await Feed.findOne({ _id: _id, deleteAt: null }); },
     async getAll () { return Feed.find({ deleteAt: null }); },
-    async update (_id, feed) { return Feed.updateOne({ _id: _id }), feed, { upsert:true } ; },
-    async delete (_id) { return Feed.updateOne({ _id: _id }), { deleteAt: Date.now() }, { upsert:true } ;  }
+    async update (_id, feed) {
+        feed.updateAt = Utils.dateNowSQL();
+        Feed.updateOne({ _id: _id }), feed, { upsert:true } ;
+    },
+    async delete (_id) { Feed.updateOne({ _id: _id }), { deleteAt: Utils.dateNowSQL() }, { upsert:true } ;  }
 };
 
 module.exports = feedRepository;
