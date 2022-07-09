6717937991930c2fe1bad50176cba76737b9c7b8
Christian <chsanleo@gmail.com>
2022-07-09 19:09:52 +0200

    create method to convert data to model feed

--- core/model/conversionToModel.js
+++ core/model/conversionToModel.js
@@ -1,6 +1,17 @@
 
 const conversionToModel = {
+    data_To_feed(data){
+        let feed = {
+            title: data.title,
+            body: data.body,
+            image: data.image,
+            imageLink: data.imageLink,
+            source: data.source,
+            publisher: data.publisher
+        }
 
+        return feed;
+    }
 };
 
 module.exports = conversionToModel;
