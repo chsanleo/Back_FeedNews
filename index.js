48f2d6f9e0acd480169b391096c0839b59df384b
Christian <chsanleo@gmail.com>
2022-07-10 01:18:06 +0200

    delete regions comments

--- index.js
+++ index.js
@@ -2,36 +2,27 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-//#region Load_Logger
 const logger = require('./config/log4js-config.js');
 var log4js = require('log4js');
-
 app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'auto' }));
-//#endregion
 
-//#region Load_env
+
 require('dotenv').config();
-//#endregion
 
-//#region Load_DB
+
 const dbconnect = require('./core/database/config/mongoDb.js');
 dbconnect();
-//#endregion
 
-//#region Load Router
+
 const feedRouter = require('./routers/feedRouter.js');
-//#endregion
 
 
-//#region app
 app.use(cors());
 app.use(express.json());
-//#endregion
 
 
 //#region 
 app.use('/feed', feedRouter);
-//#endregion
 
 
 let port = process.env.PORT;
