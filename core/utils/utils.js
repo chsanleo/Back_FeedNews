4c4023534289c4f9573e8dc3604d12e2900550d5
Christian <chsanleo@gmail.com>
2022-07-09 18:52:30 +0200

    modify isEmpty to consider more cases

--- core/utils/utils.js
+++ core/utils/utils.js
@@ -1,6 +1,6 @@
 const Utils = {
     isNullOrEmpty(variable) {
-        return (variable == null || variable === '' || variable === ' ' || variable === undefined);
+        return (variable === null || variable.trim() === this.stringEmpty() || variable === undefined);
     },
     isInt(value) { return !isNaN(value) && !isNaN(parseInt(value, 10)); },
     isFloat(value) { return Number(value) === value && value % 1 !== 0; },
