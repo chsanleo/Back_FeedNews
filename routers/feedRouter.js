84693b514baf371d2dacae7be9f448ede9557a4d
Christian <chsanleo@gmail.com>
2022-07-09 19:14:33 +0200

    create routers to feedController method

--- routers/feedRouter.js
+++ routers/feedRouter.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 const feedController = require('../core/controllers/feedController.js');
 
+router.post('/create', feedController.create);
+router.post('/', feedController.get);
+router.get('/', feedController.getAll);
+router.post('/update', feedController.update);
+router.post('/delete', feedController.delete);
 
 module.exports = router;
