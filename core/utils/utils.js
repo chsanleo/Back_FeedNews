ff4ec450490c4392acdf365908c6632b1dab42ec
Christian <chsanleo@gmail.com>
2022-07-09 18:51:54 +0200

    add method to stringEmpty

--- core/utils/utils.js
+++ core/utils/utils.js
@@ -4,6 +4,7 @@ const Utils = {
     },
     isInt(value) { return !isNaN(value) && !isNaN(parseInt(value, 10)); },
     isFloat(value) { return Number(value) === value && value % 1 !== 0; },
+    stringEmpty() { return ''; }
 };
 
 module.exports = Utils;
