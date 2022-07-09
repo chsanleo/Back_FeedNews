537557a2d1853ec8e6e759bf0559f97f4cc50ad5
Christian <chsanleo@gmail.com>
2022-07-09 19:12:18 +0200

    import conversions to model and to return

--- core/controllers/feedController.js
+++ core/controllers/feedController.js
@@ -1,4 +1,6 @@
 const _feedService = require('../services/feedService.js');
+const conversionToModel = require('../model/conversionToModel.js');
+const conversionReturn = require('../model/conversionToReturn.js');
 
 const feedController = {
    
