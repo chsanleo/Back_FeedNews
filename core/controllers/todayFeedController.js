6c7e22e7292959033c100049c89131e8fde38550
Christian <chsanleo@gmail.com>
2022-07-10 01:37:39 +0200

    create controller for today

---
+++ core/controllers/todayFeedController.js
@@ -0,0 +1,9 @@
+const _feedService = require('../services/feedService.js');
+const conversionReturn = require('../model/conversionToReturn.js');
+
+
+const feedTodayController = {
+
+};
+
+module.exports = feedTodayController;
