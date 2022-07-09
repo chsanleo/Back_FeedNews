7b52fc74a1a5eac3114f2e799bf58f98ac76a1ad
Christian <chsanleo@gmail.com>
2022-07-09 19:13:49 +0200

    create getAll method for feedController

--- core/controllers/feedController.js
+++ core/controllers/feedController.js
@@ -24,6 +24,18 @@ const feedController = {
             res.status(500).send({ message: errorMsg });
         }
     },
+    async getAll(req, res){
+        try {
+            let feedList = [];
+            for (let feed of await _feedService.getAll()) {
+                feedList.push(conversionReturn.feed_To_return(feed));
+            }
+            res.status(202).send(feedList);
+        } catch (error) {
+            global.loggererror.error(error.message);
+            res.status(500).send({ message: errorMsg });
+        }
+    },
 };
 
 module.exports = feedController;
