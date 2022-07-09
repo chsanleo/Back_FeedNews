3f17777ad3bb5ce422b4babd8dcd3bb485ae74df
Christian <chsanleo@gmail.com>
2022-07-10 01:40:35 +0200

    FIX type of call post => get

--- routers/feedToday.js
+++ routers/feedToday.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const feedTodayController = require('../core/controllers/todayFeedController.js');
 
-router.post('/today', feedTodayController.getAllToday);
+router.get('/today', feedTodayController.getAllToday);
 
 module.exports = router;
