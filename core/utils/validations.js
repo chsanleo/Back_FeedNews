2ce3e0e08e2dde592b3f3a7e1543260d1b97b383
Christian <chsanleo@gmail.com>
2022-07-09 18:54:19 +0200

    create isValidId for _id mongo

--- core/utils/validations.js
+++ core/utils/validations.js
@@ -1,5 +1,9 @@
-const Validations = {
+const { default: mongoose } = require('mongoose');
 
+const Validations = {
+    isValidId(_id){
+        if (!mongoose.Types.ObjectId.isValid(_id)) { throw Error(); }
+    }
 };
 
 module.exports = Validations;
