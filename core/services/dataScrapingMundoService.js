f88daac139be5d38191a0b420585dea2fab24082
Christian <chsanleo@gmail.com>
2022-07-10 17:04:09 +0200

    create file to scrap in mundo newpaper

---
+++ core/services/dataScrapingMundoService.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const Validations = require('../utils/validations.js');
+const Utils = require('../utils/utils.js');
+
+const url = "https://www.elmundo.es/";
+
+const dataScrapingMUNDOService = {
+
+};
+
+module.exports = dataScrapingMUNDOService;
