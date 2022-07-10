023f10b03d351a3a7f18a6b0480804cc9bd435ba
Christian <chsanleo@gmail.com>
2022-07-10 16:59:47 +0200

    create file to scrap data in pais newpaper

---
+++ core/services/dataScrapingPAISService.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const _feedRespository = require('../repositories/feedRepository.js');
+const Validations = require('../utils/validations.js');
+const Utils = require('../utils/utils.js');
+
+const url = "https://elpais.com";
+
+const dataScrapingPAISService = {
+
+};
+
+module.exports = dataScrapingPAISService;
