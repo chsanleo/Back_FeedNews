ec89e4c05b1362f5fadde7782d04cc4b713f332f
Christian <chsanleo@gmail.com>
2022-07-10 16:58:33 +0200

    create feedAutoController file

---
+++ core/controllers/feedAutoController.js
@@ -0,0 +1,9 @@
+const _dataScrapingService = require('../services/dataScrapingService.js');
+
+let errorMsg = 'Error, contact with the Administrator.';
+
+const feedAutoController = {
+
+};
+
+module.exports = feedAutoController;
