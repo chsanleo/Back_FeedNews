b71fd61fe0e0fa94d8281800e65554e6c42f4396
Christian <chsanleo@gmail.com>
2022-07-10 17:00:55 +0200

    create method to call scrap services and process

--- core/services/dataScrapingService.js
+++ core/services/dataScrapingService.js
@@ -3,7 +3,18 @@ const _feedRespository = require('../repositories/feedRepository.js');
 const Utils = require('../utils/utils.js');
 
 const dataScrapingService = {
-
+    async scrapingAndStorageAll() {
+        try {
+            let feedList = [];
+            feedList = await _dataScrapingPAISService.scrapeData();
+            for (let feed of feedList ) {
+                if(Utils.isEmptyObject(await _feedRespository.getByTitle(feed.title))) {
+                    await _feedRespository.create(feed);
+                }
+            }
+            
+        } catch (error) { throw Error('Create Feed -- ' + error.message); }
+    }
 };
 
 module.exports = dataScrapingService;
