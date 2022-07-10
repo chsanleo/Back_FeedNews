f98168a99a8e6e8201553bfa94630f64a0622d67
Christian <chsanleo@gmail.com>
2022-07-10 16:59:04 +0200

    create dataScrapingService file

---
+++ core/services/dataScrapingService.js
@@ -0,0 +1,9 @@
+const _dataScrapingPAISService = require('./dataScrapingPAISService.js');
+const _feedRespository = require('../repositories/feedRepository.js');
+const Utils = require('../utils/utils.js');
+
+const dataScrapingService = {
+
+};
+
+module.exports = dataScrapingService;
