6c26b914c47f88c830428d34bd9691bc0ff6093c
Christian <chsanleo@gmail.com>
2022-07-10 17:01:20 +0200

    create method to call the auto insert feed by scrapping

--- core/controllers/feedAutoController.js
+++ core/controllers/feedAutoController.js
@@ -3,7 +3,15 @@ const _dataScrapingService = require('../services/dataScrapingService.js');
 let errorMsg = 'Error, contact with the Administrator.';
 
 const feedAutoController = {
-
+    async feedAutoAll(req, res){
+        try {
+            await _dataScrapingService.scrapingAndStorageAll();
+            res.status(202).send();
+        } catch (error) {
+            global.loggererror.error(error.message);
+            res.status(500).send({ message: errorMsg });
+        }
+    }
 };
 
 module.exports = feedAutoController;
