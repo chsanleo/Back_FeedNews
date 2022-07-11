8f0ca74d6fb043ee4ab9ae4989424f65fe591842
Christian <chsanleo@gmail.com>
2022-07-11 16:00:55 +0200

    file with the test to helathcheck

---
+++ _tests_/healthCheck.spec.js
@@ -0,0 +1,17 @@
+const supertest = require('supertest');
+const { app, server } = require('../index.js');
+const request = supertest(app);
+
+describe('API helthCheck test', () => {
+  afterAll(() => {
+    server.close();
+  });
+
+  describe('GET /healthcheck', () => {
+    it('example request using a mocked database instance', async () => {
+      const res = await request.get('/healthcheck');
+
+      expect(res.status).toBe(202);
+    });
+  });
+});
