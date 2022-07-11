81e705a3fe0dc89214e48970bce6c9dba67a8f37
Christian <chsanleo@gmail.com>
2022-07-11 02:10:19 +0200

    clean code

--- core/utils/utils.js
+++ core/utils/utils.js
@@ -3,7 +3,6 @@ const Utils = {
     isEmptyString(variable) { return this.isEmpty(variable) || variable.trim() === this.stringEmpty(); },
     isEmptyObject(object) { this.isEmpty(object) || Object.keys(object) === 0; },
     isInt(value) { return !isNaN(value) && !isNaN(parseInt(value, 10)); },
-    isFloat(value) { return Number(value) === value && value % 1 !== 0; },
     stringEmpty() { return ''; },
     dateNowSQL() { return new Date().toISOString().slice(0, 19).replace('T', ' '); },
     dmySLQ() { return this.dateNowSQL().split(' ', 1).toString(); }
