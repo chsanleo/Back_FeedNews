5aedd3913ab7855415e386612929f3c771dea019
Christian <chsanleo@gmail.com>
2022-07-11 15:59:43 +0200

    add status to return helathcheck

--- routers/healthcheck.js
+++ routers/healthcheck.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Utils = require('../core/utils/utils.js')
 
-router.get('/healthcheck', (req, res) => res.send('OK  -  ' + Utils.dateNowSQL()));
+router.get('/healthcheck', (req, res) => res.status(202).send('OK  -  ' + Utils.dateNowSQL()));
 
 module.exports = router;
