db82f2d8057e689f26507f8f3bb7bfcd04a6b99d
Christian <chsanleo@gmail.com>
2022-07-13 23:56:22 +0200

    add disconnect method, export methods

--- core/database/config/mongoDb.js
+++ core/database/config/mongoDb.js
@@ -1,7 +1,7 @@
 
-const dbconnect = () => {
     const mongoose = require('mongoose');
     const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_NAMEDB}?retryWrites=true&w=majority`;
+const connectDB =  async () => {
     mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
@@ -9,5 +9,18 @@ const dbconnect = () => {
             console.log('Connection established.');
         })
         .catch(error => console.log('Error connecting:' + error));}
+};
 
-module.exports = dbconnect;
+const disconnectDB = async () => {
+    try {
+        await mongoose.connection.close();
+            if (mongod) {
+                await mongod.stop();
+            }
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+module.exports = { connectDB, disconnectDB }
