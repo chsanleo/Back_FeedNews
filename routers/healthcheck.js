afec01f848a25338b77ff51f93583f645a384409
Christian <chsanleo@gmail.com>
2022-07-14 00:05:26 +0200

    swagger to healthcheck route

--- routers/healthcheck.js
+++ routers/healthcheck.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const Utils = require('../core/utils/utils.js')
 
+/**
+ * @swagger
+ * /healthcheck:
+*    get:
+*     description: Check the online of the app 
+*     responses:
+*       202:
+*         schema:
+*           type: object
+*/
 router.get('/healthcheck', (req, res) => res.status(202).send('OK  -  ' + Utils.dateNowSQL()));
 
 module.exports = router;
